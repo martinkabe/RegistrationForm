2f738801e21148def32217477a3640a19e53afe4
Martin <martinkabe@seznam.cz>
2021-01-22 19:47:50 +0100

    Initial commit

---
+++ src/main/java/com/demo/springboot/model/Role.java
@@ -0,0 +1,20 @@
+package com.demo.springboot.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
